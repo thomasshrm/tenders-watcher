b4370eb623007d66ee3357f6fddf01b992ee2fb0
Thomas SCHIRM <thomasschirm97@gmail.com>
2025-10-25 13:27:39 +0000

    Adding ThemeProvider to ensure there will be no error with colors and inputs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './app/routes/router.tsx'
 import './index.css'
 import AppProviders from './app/routes/providers/AppProviders.tsx'
+import { ThemeProvider } from './components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppProviders>
-      <RouterProvider router={router} />
-    </AppProviders>
+    <ThemeProvider>
+      <AppProviders>
+        <RouterProvider router={router} />
+      </AppProviders>
+    </ThemeProvider>
   </StrictMode>,
 )
